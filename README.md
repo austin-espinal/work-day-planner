148f57940e5a7efbc68087d0820990105506ee31
austin espinal <austinespi@gmail.com>
2021-11-21 14:00:43 -0500

    add event persistance, updated read.me, and add site screenshot

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Work Day Scheduler Starter Code
+# Work Day Planner
+
+## Objective
+The purpose of this site is to have a work day planner in order to better organize your work day and manage time. It uses HTML, CSS, and Javascript. 
+I did not use moment.js due to it being legacy code. Instead, I used Luxon for tracking the date and current time. below is the link to the site and screenshot of it.
+
+URL:  https://austin-espinal.github.io/work-day-planner/
+
+![Work day Planner screenshot](./assets/images/work-day-planner.png)
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,27 @@
-const nineAM = $('#9AM');
-const tenAM = $("#10AM");
-// tenAM.value = 10;
-const elevenAM = $("#11AM");
-// elevenAM.value = 11;
-const twelvePM = $("#12PM");
-// twelvePM.value = 12;
-const onePM = $("#1PM");
-// onePM.value = 13;
-const twoPM = $("#2PM");
-// twoPM.value = 14;
-const threePM = $("#3PM");
-// threePM.value = 15;
-const fourPM = $("#4PM");
-// fourPM.value = 16;
-const fivePM = $("#5PM");
-// fivePM.value = 17;
+$(document).ready(function () {
+    const nineAM = $('#9AM');
+    const tenAM = $("#10AM");
+    const elevenAM = $("#11AM");
+    const twelvePM = $("#12PM");
+    const onePM = $("#1PM");
+    const twoPM = $("#2PM");
+    const threePM = $("#3PM");
+    const fourPM = $("#4PM");
+    const fivePM = $("#5PM");
 
-// var nineAMTextInput = nineAM.text;
-// var workHourArr = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+    //Decided to use LUXON instead of Moment due to Moment being legacy code
+    var DateTime = luxon.DateTime;
+    const now = DateTime.now();
 
-//Decided to use LUXON instead of Moment due to Moment being legacy code
-var DateTime = luxon.DateTime;
-const now = DateTime.now();
+    $("#currentDay").text(now.toLocaleString());
 
-$("#currentDay").text(now.toLocaleString());
+    //checks timeblocks and assigns them colors based on if the block is in the past(#d3d3d3), present(#ff6961), or future(#77dd77)
+    function timeColorChange() {
+        const currentHour = DateTime.local().hour;
 
-
-//checks timeblocks and assigns them colors based on if the block is in the past(#d3d3d3), present(#ff6961), or future(#77dd77)
-function timeColorChange() {
-    const currentHour = DateTime.local().hour;
-
-    console.log(currentHour);
-    if (currentHour < 9) {
-        nineAM.addClass("future");
+        console.log(currentHour);
+        if (currentHour < 9) {
+            nineAM.addClass("future");
             tenAM.addClass("future");
             elevenAM.addClass("future");
             twelvePM.addClass("future");
@@ -41,9 +30,9 @@ function timeColorChange() {
             threePM.addClass("future");
             fourPM.addClass("future");
             fivePM.addClass("future");
-    }
-    if (currentHour === 9) {
-        nineAM.addClass("present");
+        }
+        if (currentHour === 9) {
+            nineAM.addClass("present");
             tenAM.addClass("future");
             elevenAM.addClass("future");
             twelvePM.addClass("future");
@@ -52,10 +41,10 @@ function timeColorChange() {
             threePM.addClass("future");
             fourPM.addClass("future");
             fivePM.addClass("future");
-    }
-    
-    if (currentHour === 10) {
-        nineAM.addClass("past");
+        }
+
+        if (currentHour === 10) {
+            nineAM.addClass("past");
             tenAM.addClass("present");
             elevenAM.addClass("future");
             twelvePM.addClass("future");
@@ -64,10 +53,10 @@ function timeColorChange() {
             threePM.addClass("future");
             fourPM.addClass("future");
             fivePM.addClass("future");
-    }
-            
-    if (currentHour === 11) {
-        nineAM.addClass("past");
+        }
+
+        if (currentHour === 11) {
+            nineAM.addClass("past");
             tenAM.addClass("past");
             elevenAM.addClass("present");
             twelvePM.addClass("future");
@@ -76,9 +65,9 @@ function timeColorChange() {
             threePM.addClass("future");
             fourPM.addClass("future");
             fivePM.addClass("future");
-    }
-    if (currentHour === 12) {
-        nineAM.addClass("past");
+        }
+        if (currentHour === 12) {
+            nineAM.addClass("past");
             tenAM.addClass("past");
             elevenAM.addClass("past");
             twelvePM.addClass("present");
@@ -87,9 +76,9 @@ function timeColorChange() {
             threePM.addClass("future");
             fourPM.addClass("future");
             fivePM.addClass("future");
-    }
-    if (currentHour === 13) {
-        nineAM.addClass("past");
+        }
+        if (currentHour === 13) {
+            nineAM.addClass("past");
             tenAM.addClass("past");
             elevenAM.addClass("past");
             twelvePM.addClass("past");
@@ -98,9 +87,9 @@ function timeColorChange() {
             threePM.addClass("future");
             fourPM.addClass("future");
             fivePM.addClass("future");
-    }
-    if (currentHour === 14) {
-        nineAM.addClass("past");
+        }
+        if (currentHour === 14) {
+            nineAM.addClass("past");
             tenAM.addClass("past");
             elevenAM.addClass("past");
             twelvePM.addClass("past");
@@ -109,9 +98,9 @@ function timeColorChange() {
             threePM.addClass("future");
             fourPM.addClass("future");
             fivePM.addClass("future");
-    }
-    if (currentHour === 15) {
-        nineAM.addClass("past");
+        }
+        if (currentHour === 15) {
+            nineAM.addClass("past");
             tenAM.addClass("past");
             elevenAM.addClass("past");
             twelvePM.addClass("past");
@@ -120,9 +109,9 @@ function timeColorChange() {
             threePM.addClass("present");
             fourPM.addClass("future");
             fivePM.addClass("future");
-    }
-    if (currentHour === 16) {
-        nineAM.addClass("past");
+        }
+        if (currentHour === 16) {
+            nineAM.addClass("past");
             tenAM.addClass("past");
             elevenAM.addClass("past");
             twelvePM.addClass("past");
@@ -131,9 +120,9 @@ function timeColorChange() {
             threePM.addClass("past");
             fourPM.addClass("present");
             fivePM.addClass("future");
-    }
-    if (currentHour === 17) {
-        nineAM.addClass("past");
+        }
+        if (currentHour === 17) {
+            nineAM.addClass("past");
             tenAM.addClass("past");
             elevenAM.addClass("past");
             twelvePM.addClass("past");
@@ -142,9 +131,9 @@ function timeColorChange() {
             threePM.addClass("past");
             fourPM.addClass("past");
             fivePM.addClass("present");
-    }
-    if (currentHour > 17) {
-        nineAM.addClass("past");
+        }
+        if (currentHour > 17) {
+            nineAM.addClass("past");
             tenAM.addClass("past");
             elevenAM.addClass("past");
             twelvePM.addClass("past");
@@ -153,78 +142,78 @@ function timeColorChange() {
             threePM.addClass("past");
             fourPM.addClass("past");
             fivePM.addClass("past");
-    }
-};
-
-setInterval(timeColorChange(), (1000 * 60) * 30);
-
-// //able to create unique ids
-// newTextAreaEl.id = "textarea-" + i;
-
-// save task button click eventlistener
-$("#9AMBtn").on("click", nineSaveTask);
-$("#10AMBtn").on("click", tenSaveTask);
-$("#11AMBtn").on("click", elevenSaveTask);
-$("#12PMBtn").on("click", twelveSaveTask);
-$("#1PMBtn").on("click", oneSaveTask);
-$("#2PMBtn").on("click", twoSaveTask);
-$("#3PMBtn").on("click", threeSaveTask);
-$("#4PMBtn").on("click", fourSaveTask);
-$("#5PMBtn").on("click", fiveSaveTask);
-
-//save task function
-function nineSaveTask() {
-    alert("9AM button was clicked");
-    var nineText = $(this).siblings(".description").val();
-    localStorage.setItem("9AMTask", nineText);
-};
-function tenSaveTask() {
-    alert("10AM button was clicked");
-    var tenText = $(this).siblings(".description").val();
-    localStorage.setItem("10AMTask", tenText);
-};
-function elevenSaveTask() {
-    alert("11AM button was clicked");
-    var elevenText = $(this).siblings(".description").val();
-    localStorage.setItem("11AMTask", elevenText);
-};
-function twelveSaveTask() {
-    alert("12PM button was clicked");
-    var twelveText = $(this).siblings(".description").val();
-    localStorage.setItem("12PMTask", twelveText);
-};
-function oneSaveTask() {
-    alert("1PM button was clicked");
-    var oneText = $(this).siblings(".description").val();
-    localStorage.setItem("1PMTask", oneText);
-};
-function twoSaveTask() {
-    alert("2PM button was clicked");
-    var twoText = $(this).siblings(".description").val();
-    localStorage.setItem("2PMTask", twoText);
-};
-function threeSaveTask() {
-    alert("3PM button was clicked");
-    var threeText = $(this).siblings(".description").val();
-    localStorage.setItem("3PMTask", threeText);
-};
-function fourSaveTask() {
-    alert("4PM button was clicked");
-    var fourText = $(this).siblings(".description").val();
-    localStorage.setItem("4PMTask", fourText);
-};
-function fiveSaveTask() {
-    alert("5PM button was clicked");
-    var fiveText = $(this).siblings(".description").val();
-    localStorage.setItem("5PMTask", fiveText);
-};
-// event.preventDefault();
-//   var loadTasks = function() {
-    //     tasks = JSON.parse(localStorage.getItem("tasks"));
-    // event.preventDefault();
-    
-    function nineLoadTask() {
-        // var nineSavedText = localStorage.getItem("9AMTask");
-        $("#9AM .description").val(localStorage.getItem("9AMTask"));
+        }
     };
-$("#9AM .description").val(localStorage.getItem("9AMTask"));
+
+    //sets 30 minute interval refresh for timeColorCange function in case site is still up for extend period of time
+    setInterval(timeColorChange(), (1000 * 60) * 30);
+
+    // save task button click eventlistener
+    $("#9AMBtn").on("click", nineSaveTask);
+    $("#10AMBtn").on("click", tenSaveTask);
+    $("#11AMBtn").on("click", elevenSaveTask);
+    $("#12PMBtn").on("click", twelveSaveTask);
+    $("#1PMBtn").on("click", oneSaveTask);
+    $("#2PMBtn").on("click", twoSaveTask);
+    $("#3PMBtn").on("click", threeSaveTask);
+    $("#4PMBtn").on("click", fourSaveTask);
+    $("#5PMBtn").on("click", fiveSaveTask);
+
+    //save task functions
+    function nineSaveTask() {
+        alert("9AM button was clicked");
+        var nineText = $(this).siblings(".description").val();
+        localStorage.setItem("9AMTask", nineText);
+    };
+    function tenSaveTask() {
+        alert("10AM button was clicked");
+        var tenText = $(this).siblings(".description").val();
+        localStorage.setItem("10AMTask", tenText);
+    };
+    function elevenSaveTask() {
+        alert("11AM button was clicked");
+        var elevenText = $(this).siblings(".description").val();
+        localStorage.setItem("11AMTask", elevenText);
+    };
+    function twelveSaveTask() {
+        alert("12PM button was clicked");
+        var twelveText = $(this).siblings(".description").val();
+        localStorage.setItem("12PMTask", twelveText);
+    };
+    function oneSaveTask() {
+        alert("1PM button was clicked");
+        var oneText = $(this).siblings(".description").val();
+        localStorage.setItem("1PMTask", oneText);
+    };
+    function twoSaveTask() {
+        alert("2PM button was clicked");
+        var twoText = $(this).siblings(".description").val();
+        localStorage.setItem("2PMTask", twoText);
+    };
+    function threeSaveTask() {
+        alert("3PM button was clicked");
+        var threeText = $(this).siblings(".description").val();
+        localStorage.setItem("3PMTask", threeText);
+    };
+    function fourSaveTask() {
+        alert("4PM button was clicked");
+        var fourText = $(this).siblings(".description").val();
+        localStorage.setItem("4PMTask", fourText);
+    };
+    function fiveSaveTask() {
+        alert("5PM button was clicked");
+        var fiveText = $(this).siblings(".description").val();
+        localStorage.setItem("5PMTask", fiveText);
+    };
+
+    //load tasks
+    $("#nine-block .description").val(localStorage.getItem("9AMTask"));
+    $("#ten-block .description").val(localStorage.getItem("10AMTask"));
+    $("#eleven-block .description").val(localStorage.getItem("11AMTask"));
+    $("#twelve-block .description").val(localStorage.getItem("12PMTask"));
+    $("#one-block .description").val(localStorage.getItem("1PMTask"));
+    $("#two-block .description").val(localStorage.getItem("2PMTask"));
+    $("#three-block .description").val(localStorage.getItem("3PMTask"));
+    $("#four-block .description").val(localStorage.getItem("4PMTask"));
+    $("#five-block .description").val(localStorage.getItem("5PMTask"));
+});
